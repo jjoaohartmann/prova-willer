aba97e65df219c42acd775e2b2602c58f6199496
MARIA EDUARDA TESSARI <maria_tessari@estudante.sesisenai.org.br>
2026-08-18 08:38:41 -0300

    criação da rota post - equipes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { Prisma } from './lib/prisma.ts'
+import { prisma } from "./lib/prisma.ts"
 
 const app = express()
 const PORT = 3000
@@ -9,36 +9,44 @@ app.use(express.json())
 app.use(cors())
 
 app.post("/equipes", async (req, res) => {
+    const { nome, especialidade } = req.body;
 
+    const novaEquipe = await prisma.equipe.create ({
+        data:{
+            nome,
+            especialidade
+        }
+    })
+    return res.status(201).json ({novaEquipe})
 })
 
-app.get("/equipes", async (req, res) => {
+// app.get("/equipes", async (req, res) => {
 
-})
+// })
 
-app.post("/desenvolvedores", async (req, res) => {
+// app.post("/desenvolvedores", async (req, res) => {
 
-})
+// })
 
-app.get("/equipes/:id/desenvolvedores", async (req, req) => {
+// app.get("/equipes/:id/desenvolvedores", async (req, req) => {
 
-})
+// })
 
-app.put("/desenvolvedores", async (req, res) => {
+// app.put("/desenvolvedores", async (req, res) => {
 
-})
+// })
 
-app.put("/equipe", async (req, res) => {
+// app.put("/equipe", async (req, res) => {
 
-})
+// })
 
-app.delete("desenvolvedor/:id", async (req, res) => {
+// app.delete("desenvolvedor/:id", async (req, res) => {
 
-})
+// })
 
-app.delete("/equipe/:id", async (req, res) => {
+// app.delete("/equipe/:id", async (req, res) => {
     
-})
+// })
 
 app.listen(PORT, () => {
     console.log("API Rodando")
