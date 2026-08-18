e44de641632788c23ab92ea36dca49554be393d9
JOÃO VICTOR PRANGE HARTMANN <joao_v_hartmann@estudante.sesisenai.org.br>
2026-08-18 08:03:26 -0300

    estrutura de rotas

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import { Prisma } from './lib/prisma.ts'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.post("/equipes", async (req, res) => {
+
+})
+
+app.get("/equipes", async (req, res) => {
+
+})
+
+app.post("/desenvolvedores", async (req, res) => {
+
+})
+
+app.get("/equipes/:id/desenvolvedores", async (req, req) => {
+
+})
+
+app.put("/desenvolvedores", async (req, res) => {
+    
+})
+
+app.listen(PORT, () => {
+    console.log("API Rodando")
+})
